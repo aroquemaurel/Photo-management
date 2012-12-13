e1c9192c17e86ac5c2ebcc6641d63fc52f2b2e70
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-12-13 18:58:56 +0100

    Avancement Date_prise_de_vue

--- Date_prise_de_vue.sh
+++ Date_prise_de_vue.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 # -*- coding: UTF8 -*-
 
+#TODO supprimer les -o de la boucle
+# Constantes
+UTIL="util"
+TAGDATETIME="Exif.Photo.DateTimeOriginal"
+# Initialisation de toutes les variables
+chronologiqueView=1
+listArguments="$*"
+display=""
 # Si on a mis un -o en début ou fin des arguments
-lastArgumentVar="$`echo $#`"
-lastArgument=`eval echo $lastArgumentVar`
-
-if [ $1 = "-o" -o $lastArgument = "-o" ] 
+if [ `$UTIL/./haveArgument.sh -o $*` ]
 then
-	echo "test"
+	chronologiqueView=0 
+	for arguments
+	do
+		echo $arguments
+	done
+# TODO On trie les arguments par ordre chronologique et puis récursif
+fi
+
+for arguments
+do
+	display="$display \n$arguments : `exiv2 $arguments -g $TAGDATETIME|tr -s ' ' ';' | cut -d ';' -f 4`"
+done
+
+if [ $chronologiqueView ]
+then
+	echo $display | sort -t ':' -i -k 2
 else
-	echo "pastest"
+	echo $display 
 fi
---
+++ util/haveArgument.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# -*- coding: UTF8 -*-
+# Premier Argument => nom de la balise à chercher
+# Argument suivant, liste dans laquelle chercher la balise.
+# La balise à chercher doit être soit au début, soit à la fin des arguments.
+
+lastArgumentVar="$`echo $#`"
+lastArgument=`eval echo $lastArgumentVar`
+if [ ! $2 ]
+then
+	exit 2
+fi
+if [ $2 = "$1" -o $lastArgument = "$1" ] 
+then
+	echo test
+	exit 0 
+else
+	exit 1
+fi
+
